7bce06733cee3ed45758bb1f01fa527bec44d833
Abhik Gupta <107669163+abhik2207@users.noreply.github.com>
2023-08-27 18:43:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const apiURL = "https://type.fit/api/quotes";
+const apiURL = 'https://api.quotable.io/quotes/random';
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
 getQuote(apiURL);
 
-let number = 0;
-
 async function getQuote(url){
     const response = await fetch(url);
     var data = await response.json();
-    quote.innerHTML = data[number].text;
-    authorText = data[number].author;
-    authorText = authorText.slice(0, -10);
-    author.innerHTML = authorText;
-    if(number==15){
-        number = 0;
-    }
-    else{
-        number++;
-    }
+    quote.innerHTML = data[0].content;
+    author.innerHTML = data[0].author;
 }
 
 function tweet(){
     window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + " - by, " + author.innerHTML, "Tweet a quote!", "width=1000, height=600");
+}
+
+async function copy(){
+    await navigator.clipboard.writeText(quote.innerHTML);
+    window.alert("Text copied to clipboard!")
+}
+
+function speak(){
+    let speech = new SpeechSynthesisUtterance();
+    speech.text = quote.innerHTML
+    window.speechSynthesis.speak(speech);
 }
